fc5ff4332fe774d2c0a57a566e708b264ac70e73
birdman6450 <67883352+birdman6450@users.noreply.github.com>
2020-12-15 16:30:49 -0800

    Update latcontrol_indi.py

--- selfdrive/controls/lib/latcontrol_indi.py
+++ selfdrive/controls/lib/latcontrol_indi.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from cereal import log
 from common.realtime import DT_CTRL
-from common.numpy_fast import clip
+from common.numpy_fast import clip, interp
 from selfdrive.car.toyota.values import SteerLimitParams
 from selfdrive.car import apply_toyota_steer_torque_limits
 from selfdrive.controls.lib.drive_helpers import get_steer_max
@@ -11,32 +11,32 @@ from selfdrive.controls.lib.drive_helpers import get_steer_max
 
 class LatControlINDI():
   def __init__(self, CP):
+    self.CP = CP
     self.angle_steers_des = 0.
 
-    A = np.array([[1.0, DT_CTRL, 0.0],
-                  [0.0, 1.0, DT_CTRL],
-                  [0.0, 0.0, 1.0]])
-    C = np.array([[1.0, 0.0, 0.0],
-                  [0.0, 1.0, 0.0]])
+    A = np.matrix([[1.0, DT_CTRL, 0.0],
+                   [0.0, 1.0, DT_CTRL],
+                   [0.0, 0.0, 1.0]])
+    C = np.matrix([[1.0, 0.0, 0.0],
+                   [0.0, 1.0, 0.0]])
 
     # Q = np.matrix([[1e-2, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 10.0]])
     # R = np.matrix([[1e-2, 0.0], [0.0, 1e3]])
 
     # (x, l, K) = control.dare(np.transpose(A), np.transpose(C), Q, R)
     # K = np.transpose(K)
-    K = np.array([[7.30262179e-01, 2.07003658e-04],
-                  [7.29394177e+00, 1.39159419e-02],
-                  [1.71022442e+01, 3.38495381e-02]])
+    K = np.matrix([[7.30262179e-01, 2.07003658e-04],
+                   [7.29394177e+00, 1.39159419e-02],
+                   [1.71022442e+01, 3.38495381e-02]])
 
     self.K = K
     self.A_K = A - np.dot(K, C)
-    self.x = np.array([[0.], [0.], [0.]])
+    self.x = np.matrix([[0.], [0.], [0.]])
 
     self.enforce_rate_limit = CP.carName == "toyota"
 
     self.RC = CP.lateralTuning.indi.timeConstant
     self.G = CP.lateralTuning.indi.actuatorEffectiveness
-    self.outer_loop_gain = CP.lateralTuning.indi.outerLoopGain
     self.inner_loop_gain = CP.lateralTuning.indi.innerLoopGain
     self.alpha = 1. - DT_CTRL / (self.RC + DT_CTRL)
 
@@ -44,11 +44,16 @@ class LatControlINDI():
     self.sat_limit = CP.steerLimitTimer
 
     self.reset()
+    
+  @property
+  def outer_loop_gain(self):
+    return interp(self.v_ego, self.CP.lateralTuning.indi.outerLoopGainBP, self.CP.lateralTuning.indi.outerLoopGainV)
 
   def reset(self):
     self.delayed_output = 0.
     self.output_steer = 0.
     self.sat_count = 0.0
+    self.v_ego = 0
 
   def _check_saturation(self, control, check_saturation, limit):
     saturated = abs(control) == limit
@@ -63,8 +68,9 @@ class LatControlINDI():
     return self.sat_count > self.sat_limit
 
   def update(self, active, CS, CP, path_plan):
+    self.v_ego = CS.vEgo
     # Update Kalman filter
-    y = np.array([[math.radians(CS.steeringAngle)], [math.radians(CS.steeringRate)]])
+    y = np.matrix([[math.radians(CS.steeringAngle)], [math.radians(CS.steeringRate)]])
     self.x = np.dot(self.A_K, self.x) + np.dot(self.K, y)
 
     indi_log = log.ControlsState.LateralINDIState.new_message()
